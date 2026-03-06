31794b836f6d7bc82201a46cb168203516a976e5
anoopyadavai <anoopy5396@gmail.com>
2026-03-07 03:34:29 +0530

    Update chat_page.dart

--- chat_page.dart
+++ chat_page.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+
+  final String username;
+
+  ChatPage({required this.username});
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+
+  TextEditingController messageController = TextEditingController();
+
+  List<String> messages = [];
+
+  void sendMessage() {
+    String msg = messageController.text.trim();
+
+    if(msg.isNotEmpty){
+      setState(() {
+        messages.add("${widget.username}: $msg");
+      });
+
+      messageController.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("ChatLoop Chat")),
-      body: Center(
-        child: Text("Chat screen coming soon"),
+
+      appBar: AppBar(
+        title: Text("ChatLoop"),
       ),
-    );
-  }
-}
+
+      body: Column(
+        children: [
+
+          Expanded(
+            child: ListView.builder(
+              itemCount:
