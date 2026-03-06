b42b6fd940eaeb39183264bd241984b5ac060a8f
anoopyadavai <anoopy5396@gmail.com>
2026-03-07 03:33:38 +0530

    Update login_page.dart

--- login_page.dart
+++ login_page.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'chat_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  TextEditingController nameController = TextEditingController();
+
+  void goToChat() {
+    String username = nameController.text.trim();
+
+    if(username.isNotEmpty){
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ChatPage(username: username),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("ChatLoop Login")),
-      body: Center(
-        child: Text("Login with Username"),
+      appBar: AppBar(
+        title: Text("ChatLoop Login"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: "Enter your name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: goToChat,
+              child: Text("Enter Chat"),
+            )
+
+          ],
+        ),
       ),
     );
   }
-} 
+}
